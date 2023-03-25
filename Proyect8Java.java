e91bffa893b43e4d6dae4eed653c728c0e96a28e
yefelso <MY_NAME@example.com>
2023-03-25 09:34:55 -0500

    Proyect8_java

---
+++ Proyect8Java.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyect8.java;
+
+/**
+ *
+ * @author alumno
+ */
+public class Proyect8Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i <= 999; i++) {
+        if (i % 2 != 0) {
+                System.out.println(i);
+        }
+        }
+    }
+}
